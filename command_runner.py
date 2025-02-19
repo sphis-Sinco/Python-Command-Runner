2338f3fbec36de18a8387255e41e5a72287c8f09
Sphis-Sinco <sinconsistencia@gmail.com>
2025-02-18 18:15:09 -0600

    - Command input - use ' and not "

--- command_runner.py
+++ command_runner.py
@@ -1,10 +1,21 @@
-version = "pv1" # prototype version 1
+version = 'pv1' # prototype version 1
 
 commands = [
         {
-                "name": "Change drive",
-                "cmd": "$1:"
+                'name': 'Change drive',
+                'command': '$1:'
         }
 ]
 
 print(f'Welcome to Command Runner v.${version}')
+
+while True:
+        cmd = input('What command would you like to enter?\n> ')
+
+        i = 0
+        while i < commands.__len__():
+                if commands[i].get('name').lower() == cmd.lower():
+                        print(f'Command found! (${i}/${commands.__len__()})')
+
+                i = i + 1
+        print('Command not found')
