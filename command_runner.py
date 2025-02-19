06fb73c3c1d27f973fc84fac09ce35046e48a7c5
Sphis-Sinco <sinconsistencia@gmail.com>
2025-02-18 18:24:08 -0600

    attempt at drive switching

--- command_runner.py
+++ command_runner.py
@@ -1,3 +1,6 @@
+import subprocess
+
+
 version = 'pv2' # prototype version 2
 
 commands = [
@@ -14,13 +17,20 @@ while True:
 
         i = 0
         found_command = False
+        command = ''
 
         while i < commands.__len__():
                 if commands[i].get('name').lower() == cmd.lower():
                         print(f'Command found! ({i + 1}/{commands.__len__()})')
+                        command = cmd.lower()
                         found_command = True
 
                 i = i + 1
 
         if not found_command:
                 print('Command not found')
+
+        if found_command:
+                if command == 'change drive':
+                        drive = input('What drive would you like to switch to?\n> ')
+                        subprocess.run(f'{drive}:')
