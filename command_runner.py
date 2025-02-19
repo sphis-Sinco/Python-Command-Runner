9173352873f42402adadf4057ea08e80b314193d
Sphis-Sinco <sinconsistencia@gmail.com>
2025-02-18 18:11:32 -0600

    commands array with 1 command: change drive

--- command_runner.py
+++ command_runner.py
@@ -1,3 +1,10 @@
 version = "pv1" # prototype version 1
 
+commands = [
+        {
+                "name": "Change drive",
+                "cmd": "$1:"
+        }
+]
+
 print(f'Welcome to Command Runner v.${version}')
